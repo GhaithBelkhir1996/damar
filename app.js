07e472d6b210bad9d49e23c13620a974c78ff88d
GhaithBelkhir1996 <ghaithbelkhir@gmail.com>
2024-01-10 09:24:25 +0100

    cors

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.get('/api', (req, res) => {
   res.json({ success: false });
